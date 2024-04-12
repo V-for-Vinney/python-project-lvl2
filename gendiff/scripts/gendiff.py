2696deee240eba05eb0cc5369b88d7e9a6683c09
Владислав В <37086207+V-for-Vinney@users.noreply.github.com>
2024-04-12 19:40:44 +0300

    step 2: 'format' argument

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,26 +6,13 @@ parser = argparse.ArgumentParser(prog="gendiff", description="Compares two confi
 
 parser.add_argument("first_file")
 parser.add_argument("second_file")
+parser.add_argument("-f", "--format", help="set format of output", default="plain")
 
 
 def main():
     args = parser.parse_args()
-    print(args.first_file, args.second_file)
+    print(args.first_file, args.second_file, args.format)
 
 
 if __name__ == "__main__":
     main()
-
-"""
-gendiff -h
-usage: gendiff [-h] first_file second_file
-
-Compares two configuration files and shows a difference.
-
-positional arguments:
-  first_file
-  second_file
-
-optional arguments:
-  -h, --help            show this help message and exit
-  """
